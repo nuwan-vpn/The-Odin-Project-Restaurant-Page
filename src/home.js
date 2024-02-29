761baf78dc12fa8a8766f3b00e70fbe45ad44b74
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-02-29 20:48:27 +0530

    Add image element to restaurant homepage content
    - Create and append image element dynamically
    - Set image source and height attributes for proper display

--- src/home.js
+++ src/home.js
@@ -3,5 +3,10 @@ const createRestaurantHomePage = () =>{
     const pageContent = document.createElement('div');
     pageContent.classList.add('page-content');
 
+    //Create and append image element
+    const image = document.createElement('img');
+    image.src = './img/reataurant.jpg';
+    image.height = '300';
+    pageContent.appendChild(image);
     
 }
