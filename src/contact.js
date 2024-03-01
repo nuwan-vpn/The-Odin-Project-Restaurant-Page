56a5fcd1f0d09d3ecd16a7cf9e6bd61877696800
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-01 21:53:09 +0530

    Add function to create restaurant contact page content dynamically
    - Implement JavaScript function createRestaurantContactPage to generate contact page content
    - Select content container element using querySelector
    - Create and style pageContent div element to hold contact page content
    - Create form element for contact form and style it

---
+++ src/contact.js
@@ -0,0 +1,11 @@
+
+const createRestaurantContactPage = () =>{
+    const content = document.querySelector("#content");
+    const pageContent = document.createElement('div');
+    pageContent.classList.add('page-content');
+
+    const form = document.createElement('form');
+    form.classList.add('contact-form');
+
+    
+}
